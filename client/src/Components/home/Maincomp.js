fb79d77fefa2b67b4290e16140c3b50b7df11a9b
PansuriyaHiral <pansuriyahiral845@gmail.com>
2025-02-19 22:48:53 +0530

    solved

--- client/src/Components/home/Maincomp.js
+++ client/src/Components/home/Maincomp.js
@@ -20,30 +20,30 @@ const Maincomp = () => {
 
     return (
         <>
-            <div className="hom <img src=" https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Jupiter/Launches/T3/DesktopGateway_CategoryCard2x_758X608_T3._SY608_CB639883570_.jpg" alt="rightimg" />
-                        <a href="#">see more</a>e_section">
-            <div className="banner_part">
-                <Banner />
-            </div>
-            <div className="slide_part">
-                <div className="left_slide">
-                    <Slide title="Deal Of The Day" products={products} />
+            <div className="home_section">
+                <div className="banner_part">
+                    <Banner />
                 </div>
-                <div className="right_slide">
-                    <h4>Festive latest launches</h4>
-
+                <div className="slide_part">
+                    <div className="left_slide">
+                        <Slide title="Deal Of The Day" products={products} />
+                    </div>
+                    <div className="right_slide">
+                        <h4>Festive latest launches</h4>
+                        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Jupiter/Launches/T3/DesktopGateway_CategoryCard2x_758X608_T3._SY608_CB639883570_.jpg" alt="rightimg" />
+                        <a href="#">see more</a>
+                    </div>
                 </div>
+
+                <Slide title="Today's Deal" products={products} />
+
+                <div className="center_img">
+                    <img src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg" alt="" />
+                </div>
+
+                <Slide title="Best Seller" products={products} />
+                <Slide title="Upto 80% off" products={products} />
             </div>
-
-            <Slide title="Today's Deal" products={products} />
-
-            <div className="center_img">
-                <img src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg" alt="" />
-            </div>
-
-            <Slide title="Best Seller" products={products} />
-            <Slide title="Upto 80% off" products={products} />
-        </div >
 
             <Divider />
 
